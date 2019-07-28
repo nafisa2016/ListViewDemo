8e30fd3b040af32e50314ed7f35160142196a183
nafisar <ios.nafisa@gmail.com>
2019-07-28 20:23:25 +1000

    Initial Commit

---
+++ ListDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ListDemo
+//
+//  Created by Nafisa Rahman on 7/28/19.
+//  Copyright © 2019 com.nafisa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
